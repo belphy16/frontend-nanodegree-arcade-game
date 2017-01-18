591cb339a273d3585bccf4287aa9e5b583b84d29
BELPHY <belphy16@gmail.com>
2017-01-17 23:16:49 -0800

    updated
    changes made

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-frontend-nanodegree-arcade-game
-===============================
+#Frogger Arcade Game.
+This is a simple arcade game which I made as a part of Udacity's front-end nano degree course.
+This has made with minimum coding ,and I hope u would like playing it.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+#Important features
+1. Played using **arrow keys** of key board.
+2. Main characters are player and the bugs.
+3. Player should reach the water region for winning avoiding the bugs.
+4. Collision with bugs may lead to resetting of the game.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+###Tehnologies used:
+1.HTML5 canvas
+2.CSS.
+3.Javascript.
+
+##Launching the game:
+Launch the game after downloading the code (https://github.com/belphy16/frontend-nanodegree-arcade-game )
+
+###Happy playing
--- js/app.js
+++ js/app.js
@@ -1,19 +1,47 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//Enemy constructor
+//Enemies our player must avoid
+var Enemy = function(x, y, speed) {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.y = y;
+    this.speed = speed;
+    this.x = -4;
 };
 
-// Update the enemy's position, required method for game
+//Enemy characteristics into array
+//Creating a random Speed out of the array
+//Creating random positions out array
+
+function makeRandomSpeed() {
+
+    var speedArray = [45, 70,120,340,500,670];
+    var randSpeed = speedArray[Math.floor(Math.random() * speedArray.length)];
+    return randSpeed;
+}
+
+function makeRandomY() {
+
+    var yArray = [20,60, 90,145, 230];
+    var randY = yArray[Math.floor(Math.random() * yArray.length)];
+    return randY;
+}
+
+
+//Updating enemy if enemy crosses the screen
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+    this.x += this.speed * dt;
+    if (this.x > 600) {
+        this.reset();
+    }
+};
+
+//Resetting the enemy with the random values
+Enemy.prototype.reset = function() {
+    this.x = -2;
+    this.y = makeRandomY();
+    this.speed = makeRandomSpeed();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +49,100 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+//calling enemies ,specifying their number,speed ,positions etc from array functions
+var allEnemies = [];
+for (var i = 0; i <7 ; i++) {
+    var enemy = new Enemy(-3, makeRandomY(), makeRandomSpeed());
+    allEnemies.push(enemy);
+};
+
+
+
 // Now write your own player class
+
+
+// Player object
+
+
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+
+};
+
+//player render method
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+//player update method
+Player.prototype.update = function() {
+
+    if (this.y === -25) {
+        //reset if player on water
+        //enables to start a new game after winning
+        this.reset();
+    } else if (this.y > 400)
+    //player can not go lower than starting position
+    //player cannot move out of screen
+    {
+        this.y = 400;
+    } else if (this.x > 450)
+    //if the player passed 450 on the right side, let player appear again on left side
+    //this prevents player from moving out of screen
+    {
+        this.x = -50;
+    } else if (this.x < -50)
+    //if the player passes - 50 on the left side, let player appear again on right side
+    //this prevents player from moving out of screen
+    {
+        this.x = 450;
+    } else {
+
+        for (var i = 0; i < allEnemies.length; i++) {
 
 
+            if ((allEnemies[i].x > this.x - 70 && allEnemies[i].x < this.x + 50) && (allEnemies[i].y == this.y))
+            {
+                this.reset();
+            }
+        }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+    }
+};
+
+
+//player handle input method
+Player.prototype.handleInput = function(key) {
+    if (key === 'up') {
+        this.y -= 85;
+        console.log("Up");
+    } else if (key === 'down') {
+        this.y += 85;
+        console.log("Down");
+    } else if (key === 'left') {
+        this.x -= 50;
+        console.log("Left");
+    } else if (key === 'right') {
+        this.x += 50;
+        console.log("Right");
+    }
+};
+
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +153,8 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+// Place the player object in a variable called player
+
+var player = new Player(200, 400);
